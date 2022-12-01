2f592bacf80f5f18aacb9649cca9bc947f53cfb0
unknown <tim@tecreations.ca>
2022-12-01 14:30:46 -0700

    new cycle

---
+++ app/ConfigConsumerApplication.java
@@ -0,0 +1,16 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ *
+ * @author Tim
+ */  
+@SpringBootApplication
+public class ConfigConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigConsumerApplication.class,args);
+    }
+       
+}
